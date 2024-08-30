4573b6a9f37b7cdc0ff558685d14b93fd5bd3a5c
linikitaa <linikitaa@yandex.ru>
2024-08-30 18:21:26 +0300

    Refactoring

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "17d15ae43ca977a4"; }
+/******/ 		__webpack_require__.h = function() { return "9920e8d9667f9221"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
